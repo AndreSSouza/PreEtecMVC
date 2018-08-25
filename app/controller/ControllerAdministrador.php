1249a186b4b767ac03cad51d02c9cea1a4d8561c
ANDRELUCASDASILVASOU <ANDRELUCASDASILVASOU@DESKTOP-81NJOA4>
2018-08-25 15:23:34 -0300

    exclui algumas coisas desnecessarias e ja endereçei os menus

--- app/controller/ControllerAdministrador.php
+++ app/controller/ControllerAdministrador.php
@@ -1,17 +1,17 @@
-<?php
-namespace App\Controller;
-
-use Src\Classes\ClassRender;
-use Src\Interfaces\InterfaceView;
-
-class ControllerAdministrador extends ClassRender implements InterfaceView {
-    
-    public function __construct() {
-        $this->setTitle("Área Administrativa");
-        $this->setDescription("Área administrativa da Pré-Etec");
-        $this->setKeywords("");
-        $this->setDir("admin/");
-        $this->renderLayout();                
-    }
-    
-}
+<?php 
+//namespace App\Controller;
+//
+//use Src\Classes\ClassRender;
+//use Src\Interfaces\InterfaceView;
+//
+//class ControllerAdministrador extends ClassRender implements InterfaceView {
+//    
+//    public function __construct() {
+//        $this->setTitle("Área Administrativa");
+//        $this->setDescription("Área administrativa da Pré-Etec");
+//        $this->setKeywords("");
+//        $this->setDir("admin/");
+//        $this->renderLayout();                
+//    }
+//    
+//}
--- app/controller/ControllerInscricao.php
+++ app/controller/ControllerInscricao.php
@@ -1,6 +1,35 @@
 <?php
 namespace App\Controller;
 
-class ControllerIncricao {
+use Src\Classes\ClassRender;
+use Src\Interfaces\InterfaceView;
+
+class ControllerInscricao extends ClassRender implements InterfaceView {
+    
+    public function __construct() {
+        $this->setTitle("Lista de Espera");
+        $this->setDescription("Área responsavel por cadastrar, visualizar e excluir os inscritos");
+        $this->setKeywords("lista de espera", "inscrição", "");
+        $this->setDir("inscricao/");
+        $this->renderLayout(); 
+    }
     
 }
+
+//<?php
+//namespace App\Controller;
+//
+//use Src\Classes\ClassRender;
+//use Src\Interfaces\InterfaceView;
+//
+//class ControllerAdministrador extends ClassRender implements InterfaceView {
+//    
+//    public function __construct() {
+//        $this->setTitle("Área Administrativa");
+//        $this->setDescription("Área administrativa da Pré-Etec");
+//        $this->setKeywords("");
+//        $this->setDir("admin/");
+//        $this->renderLayout();                
+//    }
+//    
+//}
